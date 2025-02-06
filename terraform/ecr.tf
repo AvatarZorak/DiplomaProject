7930dcea7a32fb592506376e4afeb634dfd97607
AvatarZorak <alexhristov2005@gmail.com>
2025-02-06 18:25:27 +0200

    Removed unwanted file

--- terraform/ecr.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "frontend" {
-  name = "ah-frontend-repository"
-  image_tag_mutability = "MUTABLE"
-}
-
-resource "aws_ecr_repository" "backend" {
-  name = "ah-backend-repository"
-  image_tag_mutability = "MUTABLE"
-}
